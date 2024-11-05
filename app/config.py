0ae3e79f630a791203defe454d38e9812eab76a5
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-11-05 22:15:31 +0400

    install pydantic-settings. create config file

---
+++ app/config.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).parent
+ENV_FILE_PATH = BASE_DIR / ".env"
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE_PATH,
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    def get_db_url(self):
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+
+settings = Settings()
+
+if __name__ == "__main__":
+    print(ENV_FILE_PATH)
+    print(settings.model_dump())
+    print(settings.get_db_url())
